a8c8e29b2faa8b70f5b87d9c35f47df068d6735d
rasraz <rassraz1379@gmail.com>
2023-10-21 19:42:26 +0330

    (add):models DB

--- InsuranceAPI/models.py
+++ InsuranceAPI/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class HouseModel(models.Model):
+    OWNERSHIP_STATUS_CHOICE=(('owned','صاحب'),('mortgage','رهن'))
+    ownership_status=models.CharField(max_length=10,choices=OWNERSHIP_STATUS_CHOICE)
 
+class VehicleModel(models.Model):
+    year=models.DateField()
+
+class DataModel(models.Model):
+    MARITAL_STATUS_CHOICE=(('married','متاهل'),('Single','مجرد'))
+    age=models.CharField(max_length=3)
+    dependents=models.CharField(max_length=16)
+    income=models.CharField(max_length=16)
+    marital_status=models.CharField(max_length=8,choices=MARITAL_STATUS_CHOICE)
+    risk_questions=models.CharField(max_length=1)
+    house=models.ForeignKey(to=HouseModel,on_delete=models.CASCADE,null=True,blank=True,related_name='data')
+    vehicle=models.ForeignKey(to=VehicleModel,on_delete=models.CASCADE,null=True,blank=True,related_name='data')
