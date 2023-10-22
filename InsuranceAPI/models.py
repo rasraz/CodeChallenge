ad5a264eb85103ee841260939c0d5ce74c7fdcb8
rasraz <rassraz1379@gmail.com>
2023-10-22 04:02:36 +0330

    (update):API debugging

--- InsuranceAPI/models.py
+++ InsuranceAPI/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Model for recording and storing user insurance risk data
-class DataModel(models.Model):
-    MARITAL_STATUS_CHOICE=(('married','married'),('Single','Single'))
-    OWNERSHIP_STATUS_CHOICE=(('owned','owned'),('mortgage','mortgage'))
-    age=models.CharField(max_length=3)
-    dependents=models.CharField(max_length=16)
-    income=models.CharField(max_length=16)
-    marital_status=models.CharField(max_length=8,choices=MARITAL_STATUS_CHOICE)
-    risk_questions=models.CharField(max_length=1)
-    house=models.CharField(max_length=10,null=True,blank=True,choices=OWNERSHIP_STATUS_CHOICE)
-    vehicle=models.DateField(null=True,blank=True)
+# class DataModel(models.Model):
+#     MARITAL_STATUS_CHOICE=(('married','married'),('Single','Single'))
+#     OWNERSHIP_STATUS_CHOICE=(('owned','owned'),('mortgage','mortgage'))
+#     age=models.CharField(max_length=3)
+#     dependents=models.CharField(max_length=16)
+#     income=models.CharField(max_length=16)
+#     marital_status=models.CharField(max_length=8,choices=MARITAL_STATUS_CHOICE)
+#     risk_questions=models.CharField(max_length=1)
+#     house=models.CharField(max_length=10,null=True,blank=True,choices=OWNERSHIP_STATUS_CHOICE)
+#     vehicle=models.DateField(null=True,blank=True)
--- InsuranceAPI/views.py
+++ InsuranceAPI/views.py
@@ -1,37 +1,46 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import json
 from .serializers import RiskCalculationSerializer
 
 # View API registration and calculation of user insurance risk
 class RiskCalculationView(APIView):
     # Method for calculating user insurance risk
-    def calculation(self,data):
+    def calculation(self,validated_data):
+        risk_questions=sum(validated_data.get('risk_questions'))
+        income=int(validated_data.get('income'))
+        vehicle = data.get('vehicle', {}).get('year', None)
+        house = data.get('house', {}).get('ownership_status', None)
+        age=int(validated_data.get('age'))
+        marital_status=validated_data.get('marital_status')
         scores={
-            'auto':0,
-            'life':0,
-            'home':0,
-            'disability':0,
+            'auto':risk_questions,
+            'life':risk_questions,
+            'home':risk_questions,
+            'disability':risk_questions,
         }
-        if (int(data.income==0)) and (vehicle is null) and (house is null):
+        if (int(income==0)) and (vehicle is None) and (house is None):
             del scores['disability']
             del scores['home']
             del scores['auto']
-        if (60<int(data.age)):
+        if (60<int(age)):
             if 'disability' in scores:del scores['disability']
             del scores['life']
-        if (int(data.age)<30):
+        if (int(age)<30):
             for key in scores:scores[key] -= 2
-        elif (int(data.age)<40) and (30<int(data.age)):
+        elif (int(age)<40) and (30<int(age)):
             for key in scores:scores[key] -= 1
-        if (data.house=='mortgage'):
+        if (house=='mortgage'):
             if 'home' in scores:scores['home']+=1
             if 'disability' in scores:scores['disability']+=1
-        if (200_000<int(data.income)):
+        if (200_000<int(income)):
             for key in scores:scores[key] -= 1
-        if (data.marital_status=='married'):
+        if (marital_status=='married'):
             if 'life' in scores:scores['life']+=1
             if 'disability' in scores:scores['disability']-=1
-        year=int(data.vehicle.year)
+        year=int(vehicle)
         today = int(datetime.now().year)
         if (5<(today-year)):
             if 'auto' in scores:scores['auto']+=1
@@ -47,9 +56,10 @@ class RiskCalculationView(APIView):
     # Post API to receive information sent by the user
     def post(self,request):
         serialized_data=RiskCalculationSerializer(data=request.data)
+        print(json.loads(request.data['risk_questions']))
         if serialized_data.is_valid():
-            serialized_data.save()
-            data_calculated=self.calculation(serialized_data.data)
+            # serialized_data.save()
+            data_calculated=self.calculation(validated_data=serialized_data.data)
             return Response(data_calculated, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
